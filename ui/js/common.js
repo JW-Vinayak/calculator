6bff99ca354ad2f293bc9c4f83dfb680476dc1b2
JW-Vinayak <vinayak@junowallet.com>
2015-01-06 12:46:27 +0530

    restructured calculator logic to be handled by service.

--- ui/js/common.js
+++ ui/js/common.js
@@ -2,195 +2,95 @@
 
 var app = angular.module('calculator',[]);
 
-app.service("TempCalc", function(){
-  var result = flow = "";
-    this.perform = function(val) {
-    this.result = val;
-    console.log("service result : " + this.result);
-  };
-});
-
-app.controller('TempCalculatorCtrl',function($scope,TempCalc ){
-  $scope.flow ="";
-  $scope.result = "";
-  this.buttons = ["7","8","9","/","4","5","6","*","1","2","3","-",".","0","=","+"];
-  this.perform = function(val){
-    //$scope.result = val;
-    TempCalc.perform(val);
-    //console.log("ctrl result " + $scope.result);
-    //console.log($scope.result);
-  };
-});
-
 app.service("CalcService", function(){
-  this.var = "";
-  this.flow = "";
-  this.result = "";
+  var result = "0",
+      flow = "";
   this.currentOp = "";
-  this.resultEl = document.getElementById('result');
-  this.flowEl = document.getElementById('flow');
   this.opChangeFlag = false;
   this.resultFlag = false;
-  this.perform = function(val, flow, result){
-    this.flow = flow;
-    this.result = result;
-    if(["1","2","3","4","5","6","7","8","9","0","."].indexOf(val) > -1){
-      if (this.resultFlag) {
-        this.resultFlag = false;
-        this.reset();
-        this.resultEl.innerHTML = val;
-        this.result = val;
-      }
-      else if(this.opChangeFlag) {
-        this.opChangeFlag = false;
-        this.resultEl.innerHTML = val;
-        this.result = val;
-      }
-      else {
-        if(this.resultEl.innerHTML == "0" && val !== ".") {
-          this.resultEl.innerHTML = val;
-          this.result = val;
-        }
-        else {
-          if(val == "." && this.resultEl.innerHTML.toString().indexOf(".") > -1)
-            return false;
-            this.resultEl.innerHTML = this.resultEl.innerHTML + val;
-            this.result = this.resultEl.innerHTML;
-          }
-      }
-    }
-    else if(["+","-","*","/"].indexOf(val) > -1){
-      if(["+","-","*","/"].indexOf(this.flowEl.innerHTML.toString().charAt(this.flowEl.innerHTML.toString().length -1)) > -1 && this.resultEl.innerHTML == "0"){
-        this.flowEl.innerHTML = this.flowEl.innerHTML.substring(0,this.flowEl.innerHTML.toString().length-1) + val;
-        return false;
-      }
-      this.opChangeFlag = true;
-      if(this.resultFlag){
-        this.resultFlag = false;
-        this.flow =  this.resultEl.innerHTML + val;
-      }
-      else
-        this.flow = this.flow + this.resultEl.innerHTML + val;
-
-        this.flowEl.innerHTML = this.flow;
-        this.resultEl.innerHTML = "0";
-      }
-      else if(val == "=") {
-        this.flow = this.flow + this.resultEl.innerHTML;
-        this.flowEl.innerHTML = this.flow;
-        try {
-          var temp = eval(this.flow);
-          if(temp.toString().indexOf(".")> -1)
-            temp = temp.toFixed(4);
-            this.resultEl.innerHTML = temp ;
-            this.resultFlag = true;
-          }
-          catch(e) {
-            this.resultEl.innerHTML = "Wrong Input. Please reset.";
-          }
-        }
-    //console.log("val : " + val + " flow : " + this.flow +  " result : " + this.result);
-    };
-
-    this.reset = function() {
-      this.flow = "";
-      this.result = "";
-      this.flowEl.innerHTML = "&nbsp;";
-      this.resultEl.innerHTML = "0";
-      this.opChangeFlag = false;
-      this.resultFlag = false;
-    }
-})
-
-app.controller('CalculatorCtrl', function($scope,CalcService ){
-  //this.var = "";
-  $scope.flow = "";
-  $scope.result = "";
-  this.buttons = ["7","8","9","/","4","5","6","*","1","2","3","-",".","0","=","+"];
-  this.perform = function(val) {
-
-    CalcService.perform(val, $scope.flow, $scope.result);
-  }
-  this.reset = CalcService.reset();
-  /*
-
-  this.currentOp = "";
-  this.resultEl = document.getElementById('result');
-  this.flowEl = document.getElementById('flow');
-  this.opChangeFlag = false;
-  this.resultFlag = false;
+  this.getFlow = function() {
+    return flow;
+  };
+  this.getResult = function(){
+    return result;
+  };
+  this.reset = function() {
+    flow = "";
+    result = "0";
+    this.opChangeFlag = false;
+    this.resultFlag = false;
+  };
   this.perform = function(val){
-
-    if(["1","2","3","4","5","6","7","8","9","0","."].indexOf(val) > -1){
+    if (["1","2","3","4","5","6","7","8","9","0","."].indexOf(val) > -1) {
       if (this.resultFlag) {
         this.resultFlag = false;
         this.reset();
-        this.resultEl.innerHTML = val;
-      }
-      else if(this.opChangeFlag) {
-          this.opChangeFlag = false;
-          this.resultEl.innerHTML = val;
-      }
-      else {
-        if(this.resultEl.innerHTML == "0" && val !== ".")
-          this.resultEl.innerHTML = val;
-        else {
-          if(val == "." && this.resultEl.innerHTML.toString().indexOf(".") > -1)
+        result = val;
+      } else if (this.opChangeFlag) {
+        this.opChangeFlag = false;
+        result = val;
+      } else {
+        if (result == "0" && val !== ".") {
+          result = val;
+        } else {
+          if (val == "." && result.toString().indexOf(".") > -1)
             return false;
-          this.resultEl.innerHTML = this.resultEl.innerHTML + val;
-          this.result = this.resultEl.innerHTML;
-        }
 
+          result = result + val;
+        }
       }
-    }
-    else if(["+","-","*","/"].indexOf(val) > -1){
-      if(["+","-","*","/"].indexOf(this.flowEl.innerHTML.toString().charAt(this.flowEl.innerHTML.toString().length -1)) > -1 && this.resultEl.innerHTML == "0"){
-        this.flowEl.innerHTML = this.flowEl.innerHTML.substring(0,this.flowEl.innerHTML.toString().length-1) + val;
+    } else if (["+","-","*","/"].indexOf(val) > -1) {
+      if (["+","-","*","/"].indexOf(flow.toString().charAt(flow.toString().length -1)) > -1 && result == "0") {
+        flow = flow.substring(0,flow.toString().length-1) + val;
         return false;
       }
       this.opChangeFlag = true;
-      if(this.resultFlag){
+      if (this.resultFlag) {
         this.resultFlag = false;
-        this.flow =  this.resultEl.innerHTML + val;
-      }
-      else
-        this.flow = this.flow + this.resultEl.innerHTML + val;
+        flow =  result + val;
+      } else
+        flow = flow + result + val;
 
-      this.flowEl.innerHTML = this.flow;
-      this.resultEl.innerHTML = "0";
-    }
-    else if(val == "=") {
-      this.flow = this.flow + this.resultEl.innerHTML;
-      this.flowEl.innerHTML = this.flow;
+      result = "0";
+    } else if (val == "=") {
+      flow = flow + result;
       try {
-        var temp = eval(this.flow);
-        if(temp.toString().indexOf(".")> -1)
-          temp = temp.toFixed(4);
-        this.resultEl.innerHTML = temp ;
+        var temp = eval(flow);
+        if (temp.toString().indexOf(".")> -1)
+            temp = temp.toFixed(4);
+        result = temp;
         this.resultFlag = true;
       }
       catch(e) {
-        this.resultEl.innerHTML = "Wrong Input. Please reset.";
+        result = "Wrong Input. Please reset.";
       }
     }
   };
+});
 
+app.controller('CalculatorCtrl', function($scope,CalcService ){
+  $scope.flow =CalcService.getFlow();
+  $scope.result = CalcService.getResult();
+  this.buttons = ["7","8","9","/","4","5","6","*","1","2","3","-",".","0","=","+"];
+  this.perform = function(val) {
+    CalcService.perform(val);
+    this.updateValues();
+  };
   this.reset = function() {
-    this.flow = "";
-    this.result = "";
-    this.flowEl.innerHTML = "&nbsp;";
-    this.resultEl.innerHTML = "0";
-    this.opChangeFlag = false;
-    this.resultFlag = false;
-  }
-  */
+    CalcService.reset();
+    this.updateValues();
+  };
+  this.updateValues = function(){
+    $scope.flow =CalcService.getFlow();
+    $scope.result = CalcService.getResult();
+  };
 });
 
 app.directive('calculatorUi', function(){
   return {
     restrict : "E",
     templateUrl : "view/calculator.html",
-    controller : "TempCalculatorCtrl",
+    controller : "CalculatorCtrl",
     alias : "cctrl"
   };
 });
